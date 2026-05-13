f2ab7c81116b33f2fd5494b6cdaf75c35d9384cf
urbanoandrea886-dotcom <urbanoandrea886@gmail.com>
2026-05-13 14:58:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// MENSAJE DE BIENVENIDA
+window.onload = function(){
+
+    alert("🌊 Bienvenido a Mitología Marina 🌊");
+
+}
+
+// CAMBIAR MODO OSCURO Y CLARO
+function cambiarTema(){
+
+    document.body.classList.toggle("oscuro");
+
+}
