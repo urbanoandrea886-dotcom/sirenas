0950acc732170453ae1f0576701ef27df36b47f5
urbanoandrea886-dotcom <urbanoandrea886@gmail.com>
2026-05-22 17:55:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ window.onload = function(){
 
 }
 
-const boton = document.getElementById("modoBtn");
-
-boton.addEventListener("click", () => {
-    document.body.classList.toggle("claro");
-
-    if(document.body.classList.contains("claro")){
-        boton.textContent = "🌊 Modo Oscuro";
-    } else {
-        boton.textContent = "☀️ Modo Claro";
-    }
-});
