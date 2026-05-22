8779e9ed33d45ab8d67b29ba4d1fb029f245ca0c
urbanoandrea886-dotcom <urbanoandrea886@gmail.com>
2026-05-22 17:47:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// Variable global para contar la profundidad (empieza en cero)
 let metrosProfundidad = 0;
 
-// MENSAJE DE BIENVENIDA (No se modificó nada)
 window.onload = function(){
 
     alert("🌊 Bienvenido a Mitología Marina 🌊");
 
 }
 
-// CAMBIAR MODO OSCURO Y CLARO (Actualizado con el indicador de profundidad)
-function cambiarTema(){
+const boton = document.getElementById("modoBtn");
 
-    document.body.classList.toggle("oscuro");
-    
-    // Aumenta la profundidad en 100 metros con cada clic
-    metrosProfundidad += 100;
-    
-    // Busca el elemento en el HTML y actualiza los metros en pantalla
-    const indicador = document.getElementById("contador-metros");
-    if (indicador) {
-        indicador.innerText = metrosProfundidad;
+boton.addEventListener("click", () => {
+    document.body.classList.toggle("claro");
+
+    if(document.body.classList.contains("claro")){
+        boton.textContent = "🌊 Modo Oscuro";
+    } else {
+        boton.textContent = "☀️ Modo Claro";
     }
-}
+});
