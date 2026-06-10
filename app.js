f94b6deb94c0bac05a0add879fa8f7600d1d7438
urbanoandrea886-dotcom <urbanoandrea886@gmail.com>
2026-06-10 14:58:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ async function cargarMitos() {
             <div class="card-mito">
                 <h3>${mito.Titulo}</h3>
                 <p>${mito.cuerpo_texto}</p>
+                class="imagen-mito"
             </div>
         `;
     });
