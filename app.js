b06e680ab3a53ed099598d298626b686781eceeb
urbanoandrea886-dotcom <urbanoandrea886@gmail.com>
2026-06-15 19:34:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function cargarMitos() {
                     <h3>${mito.Titulo || "Sin título"}</h3>
 
                     <img
-                        src="${mito.imagen_url || ''}"
+                        <img src="CANTO.jpg">
                         alt="${mito.Titulo || ''}"
                         class="imagen-mito"
                     >
